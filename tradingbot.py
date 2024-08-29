2aee8e06a79ff0d8a5ee6e5f5d2c993ffdf158f3
bfjberger <bfjberger@hotmail.com>
2024-08-29 16:12:13 +0200

    trading bot with news api working

--- tradingbot.py
+++ tradingbot.py
@@ -3,10 +3,13 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime
+from alpaca_trade_api import REST
+from timedelta import Timedelta 
 
-API_KEY = "PKURAKB41NZ1BDO08M7D"
-API_SECRET = "6AsWz9yCRv3BDkqHM10Icbqsb88JL8mL5DpwUcKw"
+API_KEY = "PKBQVHG5MSFRQ898RSUF"
+API_SECRET = "BjxpbEyys4L4XsgqxLFfWbaX6ZA4QsUXyuZsvOMQ"
 BASE_URL= "https://paper-api.alpaca.markets/v2"
+
 
 ALPACA_CREDS = {
     "API_KEY":API_KEY,
@@ -20,16 +23,31 @@ class MLTrader(Strategy):
         self.sleeptime = "24H"
         self.last_trade = None
         self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
+        
         
     def position_sizing(self):
         cash = self.get_cash() 
         last_price = self.get_last_price(self.symbol)
         quantity = round(cash * self.cash_at_risk / last_price, 0)
         return cash, last_price, quantity
+    
+    def get_dates(self):
+        today = self.get_datetime()
+        three_days_prior = today - Timedelta(days=3)
+        return today.strftime('%Y-%m-%d'),  three_days_prior.strftime('%Y-%m-%d')
+    
+    def get_news(self):
+        today, three_days_prior = self.get_dates( )
+        news = self.api.get_news(symbol=self.symbol, start=three_days_prior, end=today)
+        news = [ev.__dict__["_raw"]["headline"] for ev in news]
+        return news
            
     def on_trading_iteration(self):
         cash, last_price, quantity = self.position_sizing()
         if cash > last_price:
+            news = self.get_news()
+            print(news)
             if self.last_trade == None:
                 order = self.create_order(
                     self.symbol,
